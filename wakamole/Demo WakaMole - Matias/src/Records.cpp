2a7b5be4bf3f8d0bcd93a9422333d6b8f7bdfc7f
solfamidas@gmail.com <solfamidas@gmail.com@6571c5a9-3ba3-7794-19b4-783a6db3d2b5>
2013-01-16 22:56:41 +0000

    Clase para manejo de puntuaciones

---
+++ wakamole/Demo WakaMole - Matias/src/Records.cpp
@@ -0,0 +1,90 @@
+#include "Records.h"
+
+Records::Records()
+  {
+    _vRecords.clear();
+  }
+
+Records::~Records()
+  {
+    _vRecords.clear();
+  }
+
+void Records::add ( unsigned int value )
+  {
+    _vRecords.push_back ( value );
+  }
+
+unsigned int Records::getValue ( unsigned int index )
+  {
+    int value = 0;
+
+    if ( index >= 0 && index < _vRecords.size() )
+      value = _vRecords[index];
+
+    return value;
+  }
+
+void Records::Copy ( Records &source )
+  {
+    _vRecords.clear();
+
+    for ( unsigned int i = 0; i < source.getSize(); i++ )
+      _vRecords.push_back ( source.getValue ( i ) );
+  }  
+
+Records::Records ( Records& source )
+  {
+    Copy ( source );
+  }
+
+Records Records::operator= ( Records& source )
+  {
+    Copy ( source );
+    return *this;
+  }
+
+void Records::write()
+  {
+    ofstream os( "records.dat" );
+
+    if ( !os.is_open() ) 
+      {
+        cout << "Error opening records.dat for write" << endl;
+        return;
+      }
+    else 
+      {
+        cout << "Opened points.dat for write" << endl;
+        /// Write Data
+        for ( unsigned int i = 0; i < _vRecords.size(); i++ ) {
+            os << " " << _vRecords[i];
+        }
+        os.close();
+    }
+  }
+
+ void Records::read()
+   { 
+     ifstream is("records.dat");
+     unsigned int value = 0;
+
+     if ( !is.is_open() ) 
+       {
+	 cout << "Error opening points.dat for read" << endl;
+	 return;
+       }
+     else 
+       {
+	 cout << "Opened points.dat for read" << endl;
+	 
+	 while ( !is.eof() ) 
+	   {
+	     is >> value;
+	     _vRecords.push_back ( value );
+	   }
+
+        is.close();
+    }
+
+   }
